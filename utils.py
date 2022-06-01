165bfb8b67ae08d0f9bde691b5ed2da13cd0f92a
Luong Pham Bao <58712033+19521242bao@users.noreply.github.com>
2022-06-01 17:14:47 +0700

    add utils.py

---
+++ utils.py
@@ -0,0 +1,31 @@
+import cv2 
+import os 
+import numpy as np 
+
+
+
+def order_clockwise(myPoints):
+    myPoints = myPoints.reshape((4,2))
+    myPointsNew = np.zeros((4,1,2),dtype = np.int32)
+    add = myPoints.sum(1)
+    
+    
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints,axis = 1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    return myPointsNew
+
+def biggestContour(contours):
+    biggest = np.array([])
+    max_area = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 5000:
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+    return biggest, max_area
