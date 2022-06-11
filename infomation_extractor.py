6611926de6ee4ef21259130cf27c9de1fc12963d
Luong Pham Bao <58712033+19521242bao@users.noreply.github.com>
2022-06-11 20:24:31 +0700

    update api

--- infomation_extractor.py
+++ infomation_extractor.py
@@ -5,7 +5,9 @@ import os
 import cv2 
 from PIL import Image
 from text_recognition import VietOCR
-
+from text_detect import Paddle_detection
+from paddleocr import PaddleOCR
+from preprocess_background import preprocess_background
 def processing_text(text_list):
     if len(text_list) == 0:
         return ""
@@ -16,18 +18,24 @@ class Predictor():
         self.model_reg_path='./weights/transformerocr.pth'
         self.model_detect_path='./weights/best.pt'
     def load_detect_model(self):
-        self.model_detect = torch.hub.load("ultralytics/yolov5", "custom", path = self.model_detect_path, force_reload=True)
+        self.model_detect = torch.hub.load("yolov5", "custom", path = self.model_detect_path, force_reload=True,source='local')
         self.model_detect.eval()
-        self.model_detect.conf=0.7
         
+    def load_text_detect_model(self):
+        self.model_text_detect =PaddleOCR(use_gpu=True)
     def load_reg_model(self):
         self.model_reg = VietOCR(model_path=self.model_reg_path)
     def predict(self,img_path):
+        img_folder=os.path.dirname(img_path)
+        print(img_folder)
+        #preprocess_background(img_path,"uploads")
         img=cv2.imread(img_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         
         model_detect=self.model_detect
         model_reg=self.model_reg
+        model_text_detect=self.model_text_detect
+
         # predict region of information extract
         predict = model_detect(img, size=720)    
         locate = predict.pandas().xyxy[0]
@@ -41,15 +49,46 @@ class Predictor():
         tai_ban = []
         #cắt từng vùng ảnh và thêm vảo mảng đã định danh và dự đoán dựa trên việt OCR
         for index, row in locate.iterrows():
-            
+            print(row)
             x1,x2,y1,y2 = int(row['xmin']),int(row['xmax']),int(row['ymin']),int(row['ymax'])
             crop=img[y1:y2,x1:x2]
+            #print(crop)
+            #print(x1,x2,y1,y2)
             img_crop="./crop/"+str(row["class"])+"/"+str(index)+".jpg"
             cv2.imwrite(img_crop,crop)
+            #print(img_crop)
             image=Image.open(img_crop)
+            
             text,prob=model_reg.predict(image)
-            if prob <0.6:
-                continue
+            print(prob)
+            #print(prob)
+            if float(prob) <0.7:
+                
+                result=Paddle_detection(model_text_detect,img_crop)
+                boxes= [line[0] for line in result]
+                result=""
+                img_text_detect=cv2.imread(img_crop)
+                count=0
+                print("số lượng line",boxes)
+                for box in boxes:
+                    top_left     = (int(box[0][0]), int(box[0][1]))
+                    bottom_right = (int(box[2][0]), int(box[2][1]))
+                    xmin,ymin=min(top_left[0],bottom_right[0]),min(top_left[1],bottom_right[1])
+                    xmax,ymax=max(top_left[0],bottom_right[0]),max(top_left[1],bottom_right[1])
+                    ymax=int(ymax+10)
+                    #ymin=int(ymin*1.2)
+                    
+                    crop=img_text_detect[ymin:ymax,xmin:xmax]
+                    name_img="crop/"+str(row["class"])+"/"+str(index)+"_"+str(count)+".jpg"
+                    cv2.imwrite(name_img,crop)
+
+                    image=Image.open(name_img)
+                    text_line,prob=model_reg.predict(image)
+                    result=result+text_line+" "
+                    #print(text_line)
+                    text=result
+                    count+=1
+            #print(text)
             #crop.write("crop/"+str(row['class'])+".jpg")
             #exit()
             if row['class'] == 0:
@@ -79,8 +118,10 @@ class Predictor():
             5: tai_ban
         }
         return features
-    
+
+# print(torch.cuda.is_available())
 # predictor=Predictor()
 # predictor.load_detect_model()
 # predictor.load_reg_model()
-# print(predictor.predict("uploads/0.jpg"))
+# predictor.load_text_detect_model()
+# print(predictor.predict("covers/Camera/20220331_101002.jpg"))
