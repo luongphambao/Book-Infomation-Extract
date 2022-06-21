bf745f7cbf0db828ddaa3e3938e92468da83be2f
Luong Pham Bao <58712033+19521242bao@users.noreply.github.com>
2022-06-21 22:35:58 +0700

    fix missing dấu

--- APIs/craft.py
+++ APIs/craft.py
@@ -4,11 +4,13 @@ from craft_text_detector import (
     load_craftnet_model,
     load_refinenet_model,
     get_prediction,
+    export_detected_regions
 )
 from craft_text_detector.file_utils import rectify_poly
 from PIL import Image
 import cv2
 import os
+import numpy as np
 #from text_recognition import VietOCR
 #sort chữ khi crop line để theo thứ tự từ trái sang phải
 def sort_img(regions):
@@ -25,7 +27,17 @@ def sort_img(regions):
     regions[min], regions[i] = regions[i], regions[min]
   return regions
 
-    
+def order_points(pts):
+    if isinstance(pts, list):
+        pts = np.asarray(pts, dtype='float32')
+    rect = np.zeros((4, 2), dtype='float32')
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    return rect
 def predict_craft(img, key, craft_net, refine_net):
   image = read_image(img)
                     
@@ -64,29 +76,27 @@ def predict_craft(img, key, craft_net, refine_net):
           long_size=1280
       )
   regions=prediction_result["polys"]
-  
+  regions_expand=[]
+  for box in prediction_result['boxes']:
+      box=order_points(box)
+      bl,br,tr,tl=box[0],box[1],box[2],box[3]
+      tl=[tl[0],int(tl[1]*1)]
+      tr=[int(tr[0]),int(tr[1]*1)]
+      bl=[bl[0],int(bl[1]*0.85)]
+      br=[int(br[0]),int(br[1]*0.85)]
+      regions_expand.append([bl,br,tr,tl])
+  regions_expand=np.array(regions_expand)
   #sắp xếp lại ảnh sau khi craft
-  #print(regions)
+  #regions=order_points(regions)
+  #print("regions:",regions)
   regions=sort_img(regions)
+  regions_expand=sort_img(regions_expand)
   #print("region after sort:",regions)
   a=[]
+  a_expand=[]
   for i in regions:
     a.append(rectify_poly(image, i))
-  return a
+  for i in regions_expand:
+    a_expand.append(rectify_poly(image, i))
+  return a,a_expand
   
-
-# def craft_detect():
-
-#     #load model
-#     refine_net = load_refinenet_model(cuda=True)
-#     craft_net = load_craftnet_model(cuda=True)
-    
-#     img_path="val/33.jpg" 
-#     img = Image.open(img_path)
-#     text_reg_model=VietOCR(model_path="weights/seq2seq.pth")
-#     a = read(img_path, 0, craft_net, refine_net)
-#     for i in a:
-#       text,prob=text_reg_model.predict_craft(i)
-#       if prob>0.7:
-#         print(text,prob)
-# craft_detect()
